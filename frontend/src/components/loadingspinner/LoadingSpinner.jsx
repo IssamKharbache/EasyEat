f875655f6c38e399a32e19f005dab6849143205e
lefline <issamkharbache2@gmail.com>
2024-07-02 12:20:53 +0100

    removing loading spinner

--- frontend/src/components/loadingspinner/LoadingSpinner.jsx
+++
@@ -1,6 +0,0 @@
-import { ClipLoader } from "react-spinners";
-const LoadingSpinner = ({ color, size }) => {
-  return <ClipLoader color={color ? color : "#fafafa"} size={size} />;
-};
-
-export default LoadingSpinner;
