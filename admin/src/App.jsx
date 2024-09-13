a93e5a7cf6180fd9a1a8d24f0386d48ca55922bc
lefline <issamkharbache2@gmail.com>
2024-09-13 11:39:19 +0100

    reload website on admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,6 +10,27 @@ import Dashboard from "./pages/dashboard/Dashboard";
 const App = () => {
   //base url
   const url = "https://easyeat.onrender.com";
+
+  const interval = 300000; // Interval in milliseconds (30 seconds)
+
+  function reloadWebsite() {
+    fetch(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${
+            response.status
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  }
+
+  setInterval(reloadWebsite, interval);
   return (
     <div>
       <Toaster richColors />
