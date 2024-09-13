14d75eed7c477e9f991746bbe6cba79f9e4ae293
lefline <issamkharbache2@gmail.com>
2024-09-13 11:34:22 +0100

    make a function to call api every 5 minutes(fix  render delay)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,29 @@ import MyOrders from "./pages/my-orders/MyOrders";
 const App = () => {
   //modal logins state
   const [showLogin, setShowLogin] = useState(false);
+
+  const url = `https://easyeat.onrender.com`; // Replace with your Render URL
+  const interval = 300000; // Interval in milliseconds (30 seconds)
+
+  function reloadWebsite() {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${
+            response.status
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  }
+
+  setInterval(reloadWebsite, interval);
   return (
     <>
       {showLogin ? <LoginModal setShowLogin={setShowLogin} /> : <></>}
